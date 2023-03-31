b225fd39d612303584ce9e3cae3810428ee72fa9
Armin-Saadat <armin2saadat@gmail.com>
2023-03-31 08:15:21 +0000

    debug

--- src/core/models.py
+++ src/core/models.py
@@ -157,7 +157,7 @@ class CNN_Basic(nn.Module):
         super(CNN_Basic, self).__init__()
         
         # Convolutional layers
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.conv4 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
@@ -167,16 +167,16 @@ class CNN_Basic(nn.Module):
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         
         # Fully connected layers
-        self.fc1 = nn.Linear(256 * 14 * 14, 512)
+        self.fc1 = nn.Linear(256 * 28 * 28, 512)
         self.fc2 = nn.Linear(512, 8) # 4 output points                
         
-    def forward(self, x):
+    def forward(self, x):        
         x = F.relu(self.conv1(x))
         x = self.pool(F.relu(self.conv2(x)))
         x = F.relu(self.conv3(x))
         x = self.pool(F.relu(self.conv4(x)))
-        x = self.pool(F.relu(self.conv5(x)))
-        x = x.view(-1, 256 * 14 * 14)
+        x = self.pool(F.relu(self.conv5(x)))        
+        x = x.view(-1, 256 * 28 * 28)
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+        x = self.fc2(x)                
+        return x.view(-1, 4, 2)
